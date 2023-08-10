d305dfe88d43980fc13bb978b06a20ce1dbe5c4e
请把烦恼带走 <fame_keshoakang@163.com>
2023-08-10 18:16:58 +0800

    删除多余多余代码

--- pages/common/common.js
+++ pages/common/common.js
@@ -6,28 +6,22 @@ Page({
    */
   data: {
     show: false,
-    name:'',
-    list:[
-      {name:'功能1'},
-      {name:'功能2'},
-      {name:'功能3'},
-      {name:'功能4'},
-      {name:'功能5'}
+    name: '',
+    list: [
+      { name: '功能1' },
+      { name: '功能2' },
+      { name: '功能3' },
+      { name: '功能4' },
+      { name: '功能5' }
     ],
-    listBox:[
-      {name:'功能1'},
-      {name:'功能2'},
-      {name:'功能3'},
-      {name:'功能4'},
-      {name:'功能5'}
-    ]
+    listBox: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    wx.setNavigationBarRightButton({hide: true})
+    wx.setNavigationBarRightButton({ hide: true })
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#000000',
@@ -35,23 +29,23 @@ Page({
     var listBox = [];
     for (var i = 0; i < 12; i++) {
       var letter = String.fromCharCode(65 + i);
-      var person = { name: "功能 " + letter};
+      var person = { name: "功能 " + letter };
       listBox.push(person);
     }
-this.setData({
-  listBox
-},()=>{
-  const listBox = this.data.listBox;
-    listBox[listBox.length - 1].isLast = true;
-    listBox[listBox.length - 2].isLast = true;
-    this.setData({ listBox });
-})
-  
+    this.setData({
+      listBox
+    }, () => {
+      const listBox = this.data.listBox;
+      listBox[listBox.length - 1].isLast = true;
+      listBox[listBox.length - 2].isLast = true;
+      this.setData({ listBox });
+    })
+
   },
 
   onClickShow(e) {
     const name = e.currentTarget.dataset.type.name
-    this.setData({ show: true,name });
+    this.setData({ show: true, name });
   },
 
   onClickHide(e) {
